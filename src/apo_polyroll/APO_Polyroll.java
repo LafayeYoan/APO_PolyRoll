3356b3bafe522b1bea735bf7029ad7aa6af17879
Sacha Lhopital <p1504274@EPULWKS46.univ-lyon1.fr>
2016-03-31 09:16:53 +0200

    First Commit

--- src/apo_polyroll/APO_Polyroll.java
+++ src/apo_polyroll/APO_Polyroll.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package apo_polyroll;
 
 import java.io.IOException;
@@ -19,7 +14,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author Darkos
+ * @author Yoan LAFAYE DE MICHEAUX - Sacha LHOPITAL
  */
 public class APO_Polyroll extends Application {
     
