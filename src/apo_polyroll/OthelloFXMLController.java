5037ee75575f2f80665b5e31b06497e2e40b11be
YoanLafaye <lafaye.yoan@hotmail.fr>
2016-03-31 09:33:52 +0200

    add model package

--- src/apo_polyroll/OthelloFXMLController.java
+++ src/apo_polyroll/OthelloFXMLController.java
@@ -7,7 +7,10 @@ package apo_polyroll;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 /**
  * FXML Controller class
@@ -16,13 +19,24 @@ import javafx.fxml.Initializable;
  */
 public class OthelloFXMLController implements Initializable {
 
+    @FXML GridPane othellier;
+    
+    ImageView[][] plateau;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
+        plateau = new ImageView [8][8];
+        for(int i = 0 ; i< 8; i++){
+            for(int j = 0 ; j < 8; j++){
+                ImageView image = new ImageView();
+                plateau[i][j] = image;
+                othellier.add(image,i,j);
+            }
+        }
     }    
     
 }
