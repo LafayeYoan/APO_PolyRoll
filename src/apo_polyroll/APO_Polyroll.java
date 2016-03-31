633beaebc2303dde585340095d806b26b71ca567
YoanLafaye <lafaye.yoan@hotmail.fr>
2016-03-31 09:01:35 +0200

    Ajout du plateau

--- src/apo_polyroll/APO_Polyroll.java
+++ src/apo_polyroll/APO_Polyroll.java
@@ -5,11 +5,15 @@
  */
 package apo_polyroll;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -19,26 +23,42 @@ import javafx.stage.Stage;
  */
 public class APO_Polyroll extends Application {
     
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("PolyRoll");
+
+        initRootLayout();
+    }
+    
+     /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(APO_Polyroll.class.getResource("view/OthelloFXML.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 
     /**
